7ebd54dcfca9f8971437f3883ee4d8a5abcda07b
Pham Dinh Phuong Nam <namsgu123@gmail.com>
2022-08-25 16:42:33 +0700

    update profil header

--- Instagram_clone/Views/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
+++ Instagram_clone/Views/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
@@ -7,31 +7,45 @@
 
 import UIKit
 
-class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
+final class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
         static let identifier = "ProfileInfoHeaderCollectionReusableView"
     
     private let profilePhotoImageView : UIImageView = {
        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        imageView.layer.masksToBounds = true
        return imageView
     }()
     
     private let postsButton : UIButton = {
        let button = UIButton()
+        button.backgroundColor = .secondarySystemBackground
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("Posts", for: .normal)
        return button
     }()
     
     private let followingButton : UIButton = {
        let button = UIButton()
+        button.backgroundColor = .secondarySystemBackground
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("Following", for: .normal)
        return button
     }()
     
     private let followersButton : UIButton = {
        let button = UIButton()
+        button.backgroundColor = .secondarySystemBackground
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("Followers", for: .normal)
        return button
     }()
     
     private let editProfileButton : UIButton = {
        let button = UIButton()
+        button.backgroundColor = .secondarySystemBackground
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("Edit your profile", for: .normal)
        return button
     }()
     
@@ -51,7 +65,7 @@ class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubviews()
-        backgroundColor = .systemBlue
+        backgroundColor = .systemBackground
         clipsToBounds = true
     }
     
@@ -72,6 +86,36 @@ class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+         
+        let profilePhotoSize = width/4
+        profilePhotoImageView.frame =  CGRect(x: 5,
+                                              y: 5,
+                                              width: profilePhotoSize,
+                                              height: profilePhotoSize).integral
+        profilePhotoImageView.layer.cornerRadius = profilePhotoSize/2
+        
+        let buttonHeight = profilePhotoSize/2
+        let countButtonWidth = (width - 10 - profilePhotoSize)/3
+        
+        postsButton.frame = CGRect(x: profilePhotoImageView.right,
+                                   y: 5,
+                                   width: countButtonWidth,
+                                   height: buttonHeight)
+        
+        followersButton.frame = CGRect(x: postsButton.right,
+                                   y: 5,
+                                   width: countButtonWidth,
+                                   height: buttonHeight)
+        
+        followingButton.frame = CGRect(x: followersButton.right,
+                                   y: 5,
+                                   width: countButtonWidth,
+                                   height: buttonHeight)
+        editProfileButton.frame = CGRect(x: profilePhotoImageView.right,
+                                   y: 5 + buttonHeight,
+                                   width: countButtonWidth*3,
+                                   height: buttonHeight)
+        
     }
     
     
